5a46c582d0c92d034ccc6c0af53d236b11d3abc2
Diego Martín <campos.martin.diego@gmail.com>
2023-02-09 19:37:11 +0100

    Esqueleto inicial BBDD

--- database/migrations/2023_02_09_182123_create_workgroups_table.php
+++ database/migrations/2023_02_09_182123_create_workgroups_table.php
@@ -13,16 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('workgroups', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            //$table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            //$table->rememberToken();
             $table->timestamps();
             $table->boolean('admin')->default("0"); //Para definir a que proyecto pertenece y que rol tiene
-            $table->integer('workgroup_id')->default("0")->references('id')->on('workgroups');
+            $table->integer('workgroup_id')->default("0");
         });
     }
 
@@ -33,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('workgroups');
     }
 };
--- database/migrations/2023_02_09_183105_create_projects_table.php
+++ database/migrations/2023_02_09_183105_create_projects_table.php
@@ -13,16 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            //$table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            //$table->rememberToken();
+            $table->integer('workgroup_id')->references('id')->on('workgroups');
             $table->timestamps();
-            $table->boolean('admin')->default("0"); //Para definir a que proyecto pertenece y que rol tiene
-            $table->integer('workgroup_id')->default("0")->references('id')->on('workgroups');
         });
     }
 
@@ -33,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('projects');
     }
 };
