0cdcaa232f87f3d867c2c9601e574800edc0883e
Diego Martín <campos.martin.diego@gmail.com>
2023-02-20 17:52:28 +0100

    Importar modelo log

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,12 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
-
-use Session;
+use App\Models\log;
 
 class AuthController extends Controller
 {
