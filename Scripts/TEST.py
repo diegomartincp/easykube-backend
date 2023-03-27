d5103522478b912288e22b49b527250fad39c984
Diego Martín <campos.martin.diego@gmail.com>
2023-03-27 19:31:28 +0200

    Prueba descargar y meter en fichero

--- Scripts/TEST.py
+++ Scripts/TEST.py
@@ -3,12 +3,12 @@ import sys
 import requests
 
 
-url_ek_controlplane = sys.argv[1]
-deployment = sys.argv[2]
-replicas = sys.argv[3]
+#url_ek_controlplane = sys.argv[1]
+#deployment = sys.argv[2]
+#replicas = sys.argv[3]
 
 
-r = requests.get("http://"+url_ek_controlplane+"/update_replicas?deployment="+deployment+"-deployment&replicas="+replicas)
-print(r.content)
-
+r = requests.get("http://localhost:81/postgres_backup?name=lotenemos&dbuser=diego&dbname=diegodb")
+with open('backup.sql', 'wb') as f:
+    f.write(r.content)
 
