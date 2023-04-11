9e2bb5c05aac80667ddced558b973999813abb8b
Diego <campos.martin.diego@gmail.com>
2023-04-11 12:25:59 +0200

    Se sube la imagen al repositorio de docker

--- Scripts/manejar-scripts-python.py
+++ Scripts/manejar-scripts-python.py
@@ -5,23 +5,21 @@ import shutil
 name="test"
 workgroup_id="1"
 
-#1 Copiar el fichero a la nueva ruta donde crearemos la iamgen
+#1 Copiar el fichero a la nueva ruta donde crearemos la iamgen con el dockerfile
 shutil.copy2('.\..\public\scripts\ejemplo1\script.py', '.\..\Scripts\image_creation\script.py')
 
-#1 Extraer el requirements.txt
+#2 Extraer el requirements.txt
 command="pipreqs --force .\..\Scripts\image_creation"
 ret = subprocess.run(command, capture_output=True, shell=True)
 
-#2 Ejecutar el dockerfile
+#3 Ejecutar el dockerfile
 command="docker build .\..\Scripts\image_creation -t "+name+"-"+workgroup_id
 ret = subprocess.run(command, capture_output=True, shell=True)
-print(ret)
-exit()
-command="docker tag script-test:latest diegomartinc/script-test:latest"
+
+command="docker tag "+name+"-"+workgroup_id+":latest diegomartinc/"+name+"-"+workgroup_id+":latest"
 ret = subprocess.run(command, capture_output=True, shell=True)
-print(ret)
-print("--")
-command="docker push diegomartinc/script-test:latest"
+
+command="docker push diegomartinc/"+name+"-"+workgroup_id+":latest"
 ret = subprocess.run(command, capture_output=True, shell=True)
 print(ret)
 
